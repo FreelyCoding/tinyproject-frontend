e64572cebc0a3b31f8415383c2af79518296d715
陈俊一 <chenjunyi.horse@gmail.com>
2022-09-12 00:11:30 +0800

    push

---
+++ src/views/student/studentManage.vue
@@ -0,0 +1,219 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-card class="mx-auto mt-6" max-width="90%">
+      <v-container class="text-left" v-if="!$vuetify.breakpoint.mobile">
+        <v-form ref="form" class="mb-n6" lazy-validation @keyup.enter.native="select">
+          <v-text-field
+              color="success"
+              class="search-textarea mr-3"
+              v-model="filter.id"
+              append-icon="mdi-magnify"
+              label="学号"
+              outlined
+              clearable
+              dense
+          ></v-text-field>
+          <v-text-field
+              color="success"
+              class="search-textarea mr-3"
+              v-model="filter.name"
+              append-icon="mdi-magnify"
+              label="姓名"
+              outlined
+              clearable
+              dense
+          ></v-text-field>
+          <v-text-field
+              color="success"
+              class="search-textarea mr-3"
+              v-model="filter.class"
+              append-icon="mdi-magnify"
+              label="班级"
+              outlined
+              clearable
+              dense
+          ></v-text-field>
+          <v-text-field
+              color="success"
+              item-color="success"
+              class="search-textarea mr-3"
+              append-icon="mdi-magnify"
+              v-model="filter.grade"
+              label="年级"
+              outlined
+              clearable
+              dense
+          ></v-text-field>
+          <v-btn color="success" @click="select" class="mb-1 mr-3" height="40px">筛选</v-btn>
+          <v-btn color="warning" @click="clear" class="mb-1" height="40px">清除筛选</v-btn>
+        </v-form>
+      </v-container>
+        <v-divider></v-divider>
+        <v-data-table
+            :headers="headers"
+            :items="filter.students"
+            sitem-key="name"
+            :page.sync="page"
+            :items-per-page="itemsPerPage"
+            hide-default-footer
+            disable-sort
+            calculate-widths
+        >
+          <template v-slot:[`item.action`]="{ item }">
+            <v-btn text color="primary" class="ml-n3" @click="detail(item)">
+              <v-icon small class="mr-2">mdi-pencil</v-icon>查看详情
+            </v-btn>
+          </template>
+        </v-data-table>
+        <v-divider></v-divider>
+        <v-toolbar flat class="mb-6">
+          <v-spacer></v-spacer>
+          <v-pagination color="success" v-model="page" :length="pageCount" total-visible="7"></v-pagination>
+          <v-toolbar-items v-if="!$vuetify.breakpoint.mobile" class="ml-2 mt-10">
+            <span>跳至</span>
+            <v-text-field
+                color="success"
+                v-model="jumpPage"
+                style="width: 28px; margin-top: -2px"
+                @blur="jump"
+                @keyup.enter.native="jump"
+                dense
+            ></v-text-field>
+            <span>页，每页显示</span>
+            <v-select
+                color="success"
+                item-color="success"
+                v-model="itemsPerPage"
+                :items="itemsPerPageList"
+                style="width: 50px; margin-top: -2px"
+                dense
+            ></v-select>
+            <span>条</span>
+          </v-toolbar-items>
+        </v-toolbar>
+
+      </v-card>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: 'studentManage',
+      filter: {
+        id: '',
+        name: '',
+        class: '',
+        grade: '',
+        students: [
+          {
+            "id": 1,
+            "real_name": 'Jack',
+            "grade": 2020,
+            "major": '计算机科学与技术',
+            'class': 200611,
+            'email': '114514@gmail.com',
+          }
+        ]
+      },
+      page: 1,
+      jumpPage: '',
+      pageCount: 1,
+      itemsPerPage: 15,
+      roleList: ['学生', '教员'],
+      itemsPerPageList: [5, 10, 15, 20, 30, 50],
+    }
+  },
+  methods: {
+    async jump() {
+      let next = !isNaN(parseInt(this.jumpPage, 10)) ? parseInt(this.jumpPage) : this.page;
+      next = Math.min(Math.max(1, next), this.pageCount);
+      if (next !== this.page) {
+        this.page = next;
+      }
+      this.jumpPage = '';
+    },
+    async select() {
+      await this.updateWithFix();
+    },
+    async updateWithFix() {
+      if (this.page === 1) {
+        await this.refresh();
+      } else {
+        this.page = 1;
+      }
+    },
+    hasSelect() {
+      if (this.filter.id !== null && this.filter.id !== '') {
+        return true;
+      }
+      if (this.filter.name !== null && this.filter.name !== '') {
+        return true;
+      }
+      return this.filter.role !== null && this.filter.role !== '';
+    },
+    async clear() {
+      if (this.hasSelect()) {
+        this.$refs.form.reset();
+        await this.updateWithFix();
+      }
+    },
+    detail(item) {
+      this.$router.push({
+        path: 'student-info/' + item['id'],
+        query: {
+          id: item['id'],
+          name: item['real_name'],
+          email: item['email'],
+        }
+      });
+    },
+  },
+  computed: {
+    headers() {
+      return [
+        {
+          text: '学号',
+          value: 'id'
+        },
+        {
+          text: '姓名',
+          value: 'real_name'
+        },
+        {
+          text: '年级',
+          value: 'grade'
+        },
+        {
+          text: '专业',
+          value: 'major',
+          width: '12%'
+        },
+        {
+          text: '班级',
+          value: 'class'
+        },
+        {
+          text: '用户邮箱',
+          value: 'email'
+        },
+        {
+          text: '用户管理',
+          value: 'action',
+          width: '32%'
+        }
+      ];
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search-textarea {
+  display: inline-block;
+  width: 15%;
+}
+</style>
